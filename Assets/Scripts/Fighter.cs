4c0d6326de2701ac77853d2d8277706bcbb771b0
stephanskim <39311907+stephanskim@users.noreply.github.com>
2019-03-29 10:51:58 -0400

    Add files via upload

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour {
+
+    private int health = 100;
+    private bool isAttacking = false;
+    private float horizontalSpeed = .05f;
+    private float horizTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkInput();
+	}
+
+    void checkInput() {
+        if(Input.GetKey(KeyCode.A)) {
+            if (horizTimer < horizontalSpeed) {
+                horizTimer += Time.deltaTime;
+                return;
+            }
+            horizTimer = 0;
+            transform.position += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            if (horizTimer < horizontalSpeed) {
+                horizTimer += Time.deltaTime;
+                return;
+            }
+            horizTimer = 0;
+            transform.position += Vector3.right;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public static string fighter1 = "Prefabs/GenericFighter", fighter2 = "Prefabs/GenericFighter";
+    public GameObject player1, player2;
+    public Vector2 startPos1 = new Vector2(-10.0f, -3.0f);
+    public Vector2 startPos2 = new Vector2(10.0f, -3.0f);
+
+    // Use this for initialization
+    void Start () {
+        spawnFighters();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void spawnFighters() {
+        player1 = (GameObject)Instantiate(Resources.Load(fighter1, typeof(GameObject)), startPos1, Quaternion.identity);
+        //player2 = (GameObject)Instantiate(Resources.Load(fighter2, typeof(GameObject)), startPos2, Quaternion.identity);
+    }
+
+  
+}
